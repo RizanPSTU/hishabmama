93a102eba0582e46d590a688e3c81dc94c43a9c1
Rafsan Uddin Beg Rizan <rafsanuddinbegrizan@gmail.com>
2020-03-09 16:45:33 +0600

    Flutter Project start

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# hishabmama
-Hishabmama
+hishabmama
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hishabmama/screen/loginScreen.dart';
+import 'package:hishabmama/screen/registerScreen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(new MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'HishabMama',
+        initialRoute: '/',
+        routes: {
+          '/': (context) => LoginScreen(),
+          RegisterScreen.routeName: (context) => RegisterScreen(),
+        },
+    );
+  }
+}
